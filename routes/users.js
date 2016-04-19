2f45d374557ca27e0f21e58340f63387a3f6c1aa
Zhou Zhongyuan <yawenzhongyuan@gmail.com>
2016-04-19 15:02:09 +0800

    get code success;

--- routes/users.js
+++ routes/users.js
@@ -2,9 +2,12 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  //res.send('respond with a resource');
-  res.render('users', { title: 'Express' });
+router.get('/', function (req, res, next) {
+    const code = req.query.code;
+    res.render('users', {title: '获取CODE',code:code});
+    //获取access_token
+    //ID
+    res.end();
 });
 
 module.exports = router;
