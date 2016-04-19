660dae04ee42b8282a978f885e40a6b90b4a610a
Zhou Zhongyuan <yawenzhongyuan@gmail.com>
2016-04-19 13:54:01 +0800

    get token success

--- routes/token.js
+++ routes/token.js
@@ -1,9 +1,40 @@
 var express = require('express');
 var router = express.Router();
+var tokenData = {};
+const APPID = 'wx739b9f74ab475e99';
+const SECRET = '87fd54b03415de53dffef7d3a9368fca';
+const https = require('https');
+var firstOpen = true;
+function getToken() {
+    return new Promise(function (resolve, reject) {
+        tokenData.status = 'pending';
+        var token;
+        const url = `https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=${APPID}&secret=${SECRET}`;
+        var req = https.get(url, (res) => {
+            res.on('data', (data) => {
+                data = JSON.parse(data);
+                tokenData.data = data;
+                tokenData.status = 'success';
+                resolve(token);
+            });
+        }).on('error', (e) => {
+            tokenData.status = 'error';
+            tokenData.data = e;
+            reject(e);
+        });
+    });
+}
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/', function (req, res, next) {
+    if(firstOpen){
+        getToken();
+        var refreshToken = setInterval(getToken, 2 * 60 * 60 * 1000);
+        firstOpen = false;
+    }
+    res.json(tokenData);
+    res.end();
 });
 
 module.exports = router;
+
--- routes/users.js
+++ routes/users.js
@@ -1,38 +1,9 @@
 var express = require('express');
 var router = express.Router();
-var tokenData={};
-const APPID = 'wx739b9f74ab475e99';
-const SECRET = '87fd54b03415de53dffef7d3a9368fc';
-const https = require('https');
-function getToken(){
-  return new Promise(function(resolve,reject){
-    var token;
-    const url = `https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=${APPID}&secret=${SECRET}`;
-    var req = https.get(url,(res) => {
-      res.on('data', (data) => {
-        data = JSON.parse(data);
-        token = data;
-        resolve(token);
-      });
-    }).on('error', (e) => {
-      reject(e);
-    });;
-  });
-}
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  getToken().then(function(d){
-    tokenData.data = d;
-    tokenData.status = 'success';
-    console.log(tokenData);
-  }).catch(function(e){
-    tokenData.status = 'error';
-    console.log(e)
-  });
-  res.json(tokenData);
-  res.end();
+  res.send('respond with a resource');
 });
 
 module.exports = router;
-
