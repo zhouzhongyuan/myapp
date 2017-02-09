7721329e8bc7c9d9eaf2cb2fcae2e0924e70c094
zhouzhongyuan <yawenzhongyuan@gmail.com>
2017-02-09 08:37:29 +0800

    fix invalid signature by change a customer domain

--- public/map/index.js
+++ public/map/index.js
@@ -1,7 +1,10 @@
 requirejs(["../jweixin-1.0.0.js"], function(wx) {
     const appId = 'wx739b9f74ab475e99';
-
-    fetch('/signature')
+    //添加参数url
+    let currentUrl = window.location.href;
+    currentUrl = currentUrl.split('#')[0];
+    console.log(currentUrl);
+    fetch(`/signature?url=${currentUrl}`)
         .then(function (reponse) {
             return reponse.json();
         })
@@ -17,11 +20,15 @@ requirejs(["../jweixin-1.0.0.js"], function(wx) {
                 signature: sign.signature, // 必填，签名，见附录1
                 jsApiList: ['onMenuShareTimeline', 'onMenuShareAppMessage', 'onMenuShareQQ', 'onMenuShareWeibo', 'onMenuShareQZone', 'startRecord', 'stopRecord', 'onVoiceRecordEnd', 'playVoice', 'pauseVoice', 'stopVoice', 'onVoicePlayEnd', 'uploadVoice', 'downloadVoice', 'chooseImage', 'previewImage', 'uploadImage', 'downloadImage', 'translateVoice', 'getNetworkType', 'openLocation', 'getLocation', 'hideOptionMenu', 'showOptionMenu', 'hideMenuItems', 'showMenuItems', 'hideAllNonBaseMenuItem', 'showAllNonBaseMenuItem', 'closeWindow', 'scanQRCode', 'chooseWXPay', 'openProductSpecificView', 'addard', 'chooseCard', 'openCard'], // 必填，需要使用的JS接口列表，所有JS接口列表见附录2
             });
-        }).then(function (text) {
-        wx.ready(function () {
-            // config信息验证后会执行ready方法，所有接口调用都必须在config接口获得结果之后，config是一个客户端的异步操作，所以如果需要在页面加载时就调用相关接口，则须把相关接口放在ready函数中调用来确保正确执行。对于用户触发时才调用的接口，则可以直接调用，不需要放在ready函数中。
+        })
+        .then(function (text) {
+            wx.ready(function () {
+                // config信息验证后会执行ready方法，所有接口调用都必须在config接口获得结果之后，config是一个客户端的异步操作，所以如果需要在页面加载时就调用相关接口，则须把相关接口放在ready函数中调用来确保正确执行。对于用户触发时才调用的接口，则可以直接调用，不需要放在ready函数中。
+            });
         });
-    });
+
+
+
 
     var getLocation = document.getElementById('getLocation');
     getLocation.addEventListener('click', (e) => {
@@ -33,6 +40,16 @@ requirejs(["../jweixin-1.0.0.js"], function(wx) {
                 var speed = res.speed; // 速度，以米/每秒计
                 var accuracy = res.accuracy; // 位置精度
                 console.log(latitude,longitude);
+
+                wx.openLocation({
+                    latitude,
+                    longitude,
+                    name: '上海博科', // 位置名
+                    address: '什么详情，我不知道', // 地址详情说明
+                    scale: 1, // 地图缩放级别,整形值,范围从1~28。默认为最大
+                    infoUrl: 'http://bokesoft.com' // 在查看位置界面底部显示的超链接,可点击跳转
+                });
+
             }
         });
     })
--- routes/signature.js
+++ routes/signature.js
@@ -14,8 +14,12 @@ function refreshTicket(){
 refreshTicket();
 setInterval(refreshTicket, 2 * 60 * 60 * 1000);
 router.get('/', (req, res, next)=> {
+    console.log(req.host );
+    console.log(req.url);
+    console.log(req.query.url)
     var fullUrl = req.protocol + '://' + req.get('host') + req.originalUrl;
     fullUrl = 'http://jezhang.ngrok.cc';
+    fullUrl = req.query.url;
     console.log(fullUrl);
     Data.data =  sign(jsapiTicket, fullUrl);
     Data.success = true;
